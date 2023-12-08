7eb0e42936157a2aa9e99f477bcdda16850df92f
Ankit Patel <Itsakppatel@gmail.com>
2023-12-08 18:38:38 +0530

    Add responsiveness and improved script
    Add responsiveness for mobile and improved the logic of mouseclicked function in script.js

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
 //Storing all keys & audio before because using querySelector for each key (also for unnecessary key) is not efficient
+
 const musicKeys = Array.from(document.querySelectorAll(".box"));
 
 const audio = Array.from(document.querySelectorAll("audio"));
 
+const span = document.querySelector("span");
+
 window.addEventListener("keydown", findKeyAndAudio);
 
-
 function findKeyAndAudio(e) {
-
-  
   let val;
-  if(e.key){ //This means the event comes from the keyboard
-    val=e.key
-  }else{ // This means the event comes from the cursor click
-    val = e
+  if (e.key) {
+    //This means the event comes from the keyboard
+    val = e.key;
+  } else {
+    // This means the event comes from the cursor click
+    val = e;
   }
-
+  
   const keyFound = musicKeys.find(
     (key) => key.getAttribute("data-key") === val
   );
+
   if (keyFound) {
     //If the key is found then only we need to search for the audio
     const audioFound = audio.find(
@@ -26,23 +29,38 @@ function findKeyAndAudio(e) {
     );
 
     playSound(keyFound, audioFound);
+  } else {
+    //If any other key is pressed then show it to the screen
+    span.innerText = e.key;
+    span.classList.remove("invisible");
+
+    setTimeout(removeSpan, 500);
   }
+}
+
+function removeSpan() {
+  span.classList.add("invisible");
+  span.innerText = "";
 }
 
 function playSound(currentKey, currentAudio) {
-    currentKey.classList.add("playing");
-    currentAudio.currentTime = 0;
-    currentAudio.play();
-  }
-function mouseClicked(e){ // This function is for click event
-    if(e.target.innerText.length===1){// Doing check because we do not want to pass the span innerText
-        findKeyAndAudio(e.target.innerText.toLowerCase())
-    }
+  currentKey.classList.add("playing");
+  currentAudio.currentTime = 0;
+  currentAudio.play();
 }
 
+// This function is for click event
+function mouseClicked(e) {
+
+  const keyText =
+    e.target.parentNode.getAttribute("data-key") ||
+    e.target.getAttribute("data-key");
+  
+  findKeyAndAudio(keyText);
+}
 
 const keys = document.querySelectorAll(".box");
-keys.forEach((key)=> key.addEventListener('click',mouseClicked))
+keys.forEach((key) => key.addEventListener("click", mouseClicked));
 keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
 
 function removeTransition(e) {
